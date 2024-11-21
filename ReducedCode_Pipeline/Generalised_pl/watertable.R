7b1d9c2d1f6663f605a059ed717f3045e9d829da
CharlotteHabisohn <habisohn@gmx.at>
2024-11-21 12:12:35 +0100

    meta_plots added

--- ReducedCode_Pipeline/Generalised_pl/watertable.R
+++ ReducedCode_Pipeline/Generalised_pl/watertable.R
@@ -12,3 +12,6 @@ water_readings_justValues<-water_readings_DSOnly[,-1]#brauch ich für meine Funk
 
 water_readings_DSOnly_mean_sd<-water_readings_DSOnly%>%mutate(Mean=rowMeans(.[,2:ncol(.)]))%>%mutate(SD=rowSds(as.matrix(.[,2:(ncol(.)-1)])))
 water_readings2
+
+water_mean<-water_readings_DSOnly%>%mutate(Mean=rowMeans(water_readings_DSOnly[,-1]))
+
