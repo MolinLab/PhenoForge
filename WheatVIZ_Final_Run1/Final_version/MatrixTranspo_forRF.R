02785070d536de983e20383d4fdbafdafc73690c
CharlotteHabisohn <habisohn@gmx.at>
2025-01-30 16:35:44 +0100

    last

--- WheatVIZ_Final_Run1/Final_version/MatrixTranspo_forRF.R
+++ WheatVIZ_Final_Run1/Final_version/MatrixTranspo_forRF.R
@@ -13,22 +13,3 @@ allHC_Indexcolumns_w<- reduce(list_Hc_slim,
                               inner_join,
                               by = c("Plant ID", "Measuring Date"))
 
-Genotype_allHC_Indexcolumns<-allHC_Indexcolumns_w%>%
-  inner_join(metatable_slim,
-             by="Plant ID")%>%
-  inner_join(dplyr::select(weizenernte, `Plant ID`, TG),
-             by="Plant ID")%>%
-  relocate(Genotype, Condition, .after = `Plant ID`)
-
-#alle Messtage über alle Indices in wide format
-list_Hc_interm<-allHC_Indexcolumns_w%>%
-  pivot_longer(cols = -c(`Measuring Date`, `Plant ID`),
-               names_to = "Index", values_to = "Value")
-Genotype_allHc_Datescolumns<- list_Hc_interm %>%
-  pivot_wider(names_from = `Measuring Date`, 
-              values_from = Value)%>%
-  inner_join(metatable_slim,
-             by="Plant ID")%>%
-  relocate(Genotype, Condition,.after = `Plant ID`)%>%
-  inner_join(dplyr::select(weizenernte, `Plant ID`, TG),
-             by="Plant ID")
